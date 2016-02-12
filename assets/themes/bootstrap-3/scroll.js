a379dc1a66fcfd0688a00d666c0f632eafb1ed27
Brett Weir <brett@lamestation.com>
2016-02-11 17:50:10 -0800

    Create scroll.js

---
+++ assets/themes/bootstrap-3/scroll.js
@@ -0,0 +1,18 @@
+// The function actually applying the offset
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 40);
+    }
+}
+
+// This will capture hash changes while on the page
+$(window).on("hashchange", function () {
+    offsetAnchor();
+});
+
+// This is here so that when you enter the page with a hash,
+// it can provide the offset in that case too. Having a timeout
+// seems necessary to allow the browser to jump to the anchor first.
+window.setTimeout(function() {
+    offsetAnchor();
+}, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
